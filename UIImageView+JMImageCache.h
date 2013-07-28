b4e3d392660c6ed35f09036ca5b8383e1ea9c2e8
Rodrigo García S <rod@rodrigogarcia.net>
2013-07-28 17:36:57 -0400

    Refactors JMImageCache to use queues instead of GCD to download image operations. Add convenient methods to access image on memory through NSCache

--- UIImageView+JMImageCache.h
+++ UIImageView+JMImageCache.h
@@ -12,11 +12,10 @@
 
 - (void) setImageWithURL:(NSURL *)url;
 - (void) setImageWithURL:(NSURL *)url placeholder:(UIImage *)placeholderImage;
-- (void) setImageWithURL:(NSURL *)url placeholder:(UIImage *)placeholderImage completionBlock:(void (^)(UIImage *))completionBlock;
+- (void) setImageWithURL:(NSURL *)url placeholder:(UIImage *)placeholderImage completionBlock:(void (^)(UIImage *image))completionBlock;
 - (void) setImageWithURL:(NSURL *)url placeholder:(UIImage *)placeholderImage completionBlock:(void (^)(UIImage *))completionBlock failureBlock:(void (^)(NSURLRequest *request, NSURLResponse *response, NSError* error))failure;
 - (void) setImageWithURL:(NSURL *)url key:(NSString*)key placeholder:(UIImage *)placeholderImage;
 - (void) setImageWithURL:(NSURL *)url key:(NSString*)key placeholder:(UIImage *)placeholderImage completionBlock:(void (^)(UIImage *image))completionBlock;
 - (void) setImageWithURL:(NSURL *)url key:(NSString*)key placeholder:(UIImage *)placeholderImage completionBlock:(void (^)(UIImage *image))completionBlock failureBlock:(void (^)(NSURLRequest *request, NSURLResponse *response, NSError* error))failure;
-
 
 @end
